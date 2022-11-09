e8a3c022a4a2a2c96fd1822c8d686f56e8ef0088
mmorman2 <markeith524@gmail.com>
2022-11-09 00:10:24 -0500

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>AbeFromTheAve Website</h1>
+
+      </div>
+    );
+  }
+}
